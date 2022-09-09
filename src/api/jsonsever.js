0cbba638770e8482f5d47910752b30e9faa0d3e7
momoco-git <109949855+momoco-git@users.noreply.github.com>
2022-09-09 15:33:08 +0900

    Update jsonsever.js

--- src/api/jsonsever.js
+++ src/api/jsonsever.js
@@ -10,7 +10,9 @@ export const api = axios.create({
 });
 
 axios.interceptors.request.use(
-  function (config) {},
+  function (config) {
+  return config;
+  },
   function (error) {
     // Do something with request error
     return Promise.reject(error);
