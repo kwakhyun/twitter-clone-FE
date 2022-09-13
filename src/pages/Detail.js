cb289a752f0119c341f18174760d0daa2edca16e
kwakhyun <khyun2348@naver.com>
2022-09-13 22:59:50 +0900

    Fix: api 파일 수정

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -11,12 +11,7 @@ const Detail = () => {
   const { id } = useParams();
 
   const getDetail = async (id) => {
-    return await axios.get(`http://15.164.229.25/api/auth/twit/${id}`, {
-      headers: {
-        Authorization: localStorage.getItem("access-token"),
-        "refresh-Token": localStorage.getItem("refresh-token"),
-      },
-    });
+    return await tweetAPI.getDetailTwit(id);
   };
 
   const { data } = useQuery("getDetail", getDetail(id));
