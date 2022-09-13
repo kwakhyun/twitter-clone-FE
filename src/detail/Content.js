be0e1320d4cd35a4339c13dabd9e2a2315768ccd
kwakhyun <khyun2348@naver.com>
2022-09-14 07:03:14 +0900

    Feat: 트윗 답글 기능 추가

--- src/detail/Content.js
+++ src/detail/Content.js
@@ -1,55 +1,66 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { AiOutlineRetweet } from "react-icons/ai";
 import { BiMessageRounded } from "react-icons/bi";
 import { BsBoxArrowUp } from "react-icons/bs";
-import { FaHeart } from "react-icons/fa";
+import { IoHeartOutline, IoHeart } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
-const Content = () => {
+const Content = ({ detail }) => {
   const navigate = useNavigate();
+  const [like, setLike] = useState(false);
+
   return (
     <StyledContainer>
       <StyledUserInfo>
         <img
           onClick={() => navigate("/profile")}
-          src="https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMyAg/MDAxNjA0MjI5NDA4NDMy.5zGHwAo_UtaQFX8Hd7zrDi1WiV5KrDsPHcRzu3e6b8Eg.IlkR3QN__c3o7Qe9z5_xYyCyr2vcx7L_W1arNFgwAJwg.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%8C%8C%EC%8A%A4%ED%85%94.jpg?type=w800"
-          alt="profile img"
+          src={detail?.userProfileImage}
+          alt="img"
         />
         <div>
-          <h4>nickname</h4>
-          <span>@khyun9685</span>
+          <h4>{detail?.nickname}</h4>
+          <span>@{detail?.userId}</span>
         </div>
       </StyledUserInfo>
+
       <StyledContent>
-        <p>ㅇㅇ</p>
+        {detail?.content}
+        <StyledContentImg src={detail?.fileUrl} alt="img" />
       </StyledContent>
+
       <div className="time">
-        <time>2:10 PM · Sep 10, 2022 · </time>
+        {/* <time>2:10 PM · Sep 10, 2022 · </time> */}
+        <time>{detail?.createdAt} · </time>
         <span>Twitter Web App</span>
       </div>
 
-      <StyledUserInfoBOx>
-        <div color="skyblue">
-          <StyledIconBox backcolor="skyblue">
-            <BiMessageRounded size="1.3rem" />
-          </StyledIconBox>
-        </div>
-        <div color="lightgreen">
-          <StyledIconBox backcolor="lightgreen">
-            <AiOutlineRetweet size="1.3rem" />
-          </StyledIconBox>
-        </div>
-        <div color="lightpink">
-          <StyledIconBox backcolor="lightpink">
-            <FaHeart color="red" size="1.3rem" />
-          </StyledIconBox>
+      <StyledIconDiv>
+        <div>
+          <StyledIcon color="skyblue">
+            <BiMessageRounded size="1.5rem" />
+          </StyledIcon>
         </div>
         <div>
-          <StyledIconBox backcolor="skyblue">
-            <BsBoxArrowUp size="1.3rem" />
-          </StyledIconBox>
+          <StyledIcon color="lightgreen">
+            <AiOutlineRetweet size="1.5rem" />
+          </StyledIcon>
         </div>
-      </StyledUserInfoBOx>
+        <div>
+          <StyledIcon color="lightpink" onClick={() => setLike(!like)}>
+            {like ? (
+              <IoHeartOutline size="1.5rem" />
+            ) : (
+              <IoHeart color="red" size="1.5rem" />
+            )}
+          </StyledIcon>
+        </div>
+        <div>
+          <StyledIcon color="skyblue">
+            <BsBoxArrowUp size="1.5rem" />
+          </StyledIcon>
+        </div>
+      </StyledIconDiv>
     </StyledContainer>
   );
 };
@@ -83,15 +94,22 @@ const StyledUserInfo = styled.div`
 `;
 
 const StyledContent = styled.div`
-  width: 100%;
-  margin: 0 15px;
+  display: flex;
+  flex-direction: column;
+  margin: 20px 15px 5px 15px;
   font-size: 1.5rem;
 `;
 
-const StyledUserInfoBOx = styled.div`
+const StyledContentImg = styled.img`
+  width: 90vw;
+  margin: 10px auto;
+  border-radius: 20px;
+`;
+
+const StyledIconDiv = styled.div`
   width: 90%;
   display: flex;
-  padding: 10px;
+  padding: 5px;
   margin: 15px auto 0 auto;
   align-items: center;
   flex-direction: row;
@@ -100,17 +118,16 @@ const StyledUserInfoBOx = styled.div`
   border-bottom: 1px solid #e6ecf0;
 `;
 
-const StyledIconBox = styled.text`
-  background-color: transparent;
-  width: 30px;
-  height: 30px;
+const StyledIcon = styled.text`
+  color: gray;
+  width: 40px;
+  height: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
   &:hover {
-    background-color: ${(props) => props.backcolor};
-    border-radius: 9999px;
-    color: black;
+    background-color: ${({ color }) => color};
+    border-radius: 80px;
     opacity: 0.5;
     transition: 0.5s;
   }
---
+++ src/detail/ReplyItem.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { BiMessageRounded } from "react-icons/bi";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { IoHeartOutline, IoHeart } from "react-icons/io5";
+import { BsBoxArrowUp } from "react-icons/bs";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { tweetAPI } from "../shared/api";
+import { useMutation, useQueryClient } from "react-query";
+
+const ReplyItem = ({ reply }) => {
+  const [like, setLike] = useState(false);
+
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(tweetAPI.deleteTwit, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("getDetail");
+    },
+  });
+
+  return (
+    <StyledReply>
+      <StyledProfileImg src={reply?.userProfileImage} alt="img" />
+
+      <StyledReplyContent>
+        <div className="user-info">
+          <span>
+            <span className="name">{reply?.nickname}</span>
+            <span className="id-1">@{reply?.userId}</span>
+          </span>
+          <RiDeleteBin6Line
+            className="delete"
+            onClick={() => {
+              if (window.confirm("Delete Tweet?")) {
+                mutate(reply?.id);
+              }
+            }}
+          />
+        </div>
+        <span className="id-2">
+          Replying to
+          <span> @{reply?.userId}</span>
+        </span>
+        <p className="text">{reply?.content}</p>
+        <StyledIconDiv>
+          <div>
+            <StyledIcon color="skyblue">
+              <BiMessageRounded size="1.5rem" />
+            </StyledIcon>
+          </div>
+          <div>
+            <StyledIcon color="lightgreen">
+              <AiOutlineRetweet size="1.5rem" />
+            </StyledIcon>
+          </div>
+          <div>
+            <StyledIcon color="lightpink" onClick={() => setLike(!like)}>
+              {like ? (
+                <IoHeartOutline size="1.5rem" />
+              ) : (
+                <IoHeart color="red" size="1.5rem" />
+              )}
+            </StyledIcon>
+          </div>
+          <div>
+            <StyledIcon color="skyblue">
+              <BsBoxArrowUp size="1.5rem" />
+            </StyledIcon>
+          </div>
+        </StyledIconDiv>
+      </StyledReplyContent>
+    </StyledReply>
+  );
+};
+
+const StyledReply = styled.div`
+  display: flex;
+  width: 100%;
+  border-bottom: 1px solid #e6ecf0;
+  cursor: pointer;
+`;
+
+const StyledProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin: 10px 15px;
+`;
+
+const StyledReplyContent = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: column;
+  .user-info {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    .name {
+      font-weight: bold;
+      margin-right: 5px;
+    }
+    .id-1 {
+      color: gray;
+    }
+    .delete {
+      margin-right: 5%;
+    }
+  }
+  .id-2 {
+    color: gray;
+    span {
+      font-weight: 500;
+      color: #1da1f2;
+    }
+  }
+  .text {
+    margin: 0;
+  }
+`;
+
+const StyledIconDiv = styled.div`
+  width: 95%;
+  margin: 10px 0;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledIcon = styled.text`
+  color: gray;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background-color: ${({ color }) => color};
+    border-radius: 80px;
+    opacity: 0.5;
+    transition: 0.5s;
+  }
+`;
+
+export default ReplyItem;
