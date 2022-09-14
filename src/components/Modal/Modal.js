0cbc7661cfc97bc60a4eefa219baa1464402b394
momoco-git <gnspope@naver.com>
2022-09-14 21:08:41 +0900

    Fix:모달창 스크롤

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 function Modal(props) {
   function closeModal() {
     props.closeModal();
   }
+
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY) * -1);
+    };
+  }, []);
 
   return (
     <ModalStyled onClick={closeModal}>
