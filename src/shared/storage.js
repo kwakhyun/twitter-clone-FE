06a45882c96d1fe9a31ed533a9263f900e592cac
kwakhyun <khyun2348@naver.com>
2022-09-27 08:38:15 +0900

    Design: 트윗 UI 개선 및 코드 정리

--- src/shared/storage.js
+++ src/shared/storage.js
@@ -1,27 +1,3 @@
-// import { Cookies } from "react-cookie";
-
-// const cookies = new Cookies();
-
-// export const setAccessToken = accessToken => {
-//   const today = new Date();
-//   const expireDate = today.setHours(today.getHours() + 2);
-//   return cookies.set("access_token", accessToken, {
-//     sameSite: "strict",
-//     path: "/",
-//     expires: new Date(expireDate),
-//   });
-// };
-
-// export const setRefreshToken = refreshToken => {
-//   const today = new Date();
-//   const expireDate = today.setDate(today.getDate() + 7);
-//   return cookies.set("refresh_token", refreshToken, {
-//     sameSite: "strict",
-//     path: "/",
-//     expires: new Date(expireDate),
-//   });
-// };
-
 export const getAccessToken = () => {
   return localStorage.getItem("access_token");
 };
